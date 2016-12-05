7de74da9e9eaf1593044ba251391af9bbe1a710d
Ben Woodfield <raser.13@hotmail.com>
2016-12-05 19:27:15 +0000

    Update Capitalize.py

--- Capitalize.py
+++ Capitalize.py
@@ -1,10 +1,15 @@
+# Ben Woodfield
 # Characterize level
 # Just change the FIRST letter of the word to a capital letter (uppercase)
 
 # My first answer for this worked (it solved all 11 checks)
 # But brought up errors as they wanted me to use Capitalize.
+
 string = 'the brown fox'
 print string[0].upper + string[1:]
+# What it does is say: print the first character of the string [0] as Uppercase
+# then add the rest of it from [1:] char 1 onwards
+
 
 # heres the capitalize way
 string.capitalize()

d65362e70d93198a0e41803b16223a4c83d7e986
Ben Woodfield <raser.13@hotmail.com>
2016-12-05 19:23:47 +0000

    Create flip_characters_final.py

---
+++ flip_characters_final.py
@@ -0,0 +1,17 @@
+# So heres the final version where it's wrapped in a function
+# Flip the mixed case characters in a string (eg BaNaNaNa!)
+
+def switch_case(string):
+    string_list = [] 
+    for i in string:
+        if i.isupper():
+            string_list.append(i.lower())
+        elif i.islower():
+            string_list.append(i.upper())
+        else:
+            string_list.append(i)
+
+    return(''.join(string_list))
+    
+# test with
+print switch_case("TrInKeT")
